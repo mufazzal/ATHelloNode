c31f5a56ab6863b935094a6e33899a8de8942dd3
Mufazzal <h.mufazzal@gmail.com>
2022-02-10 11:04:58 +0530

    df

--- ATInfraLaunch/ec2.tf
+++ ATInfraLaunch/ec2.tf
@@ -14,16 +14,16 @@ variable "launchTemplateName" {
 
 output "public_dns" {
   description = "ID of the VPC"
-  value       = aws_instance.Jenkin_Master.public_dns
+  value       = aws_instance.AT_ec2.public_dns
 }
 
 output "id" {
   description = "ID of the VPC"
-  value       = aws_instance.Jenkin_Master.id
+  value       = aws_instance.AT_ec2.id
 }
 
 output "public_ip" {
   description = "ID of the VPC"
-  value       = aws_instance.Jenkin_Master.public_ip
+  value       = aws_instance.AT_ec2.public_ip
 }
 
